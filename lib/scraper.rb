2692b4f1a5182ad16dccb16e571bc7dc31fe24c2
Daniel Sun <danielsun12345@gmail.com>
2020-02-26 00:39:20 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,7 +11,7 @@ class Scraper
       name = student.css(".student-name").text
       location = student.css(".student-location").text
       profile_url = student.css("a").attribute("href").value
-      student_info = {:name => name,
+      scraped_student = {:name => name,
                 :location => location,
                 :profile_url => profile_url}
       students << student_info 
